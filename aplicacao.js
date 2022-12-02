218073b13195523c0e2db7f038cb21b068fc539e
Jonatas <jonatasama@gmail.com>
2022-12-01 21:51:44 -0300

    fix

--- aplicacao.js
+++ aplicacao.js
@@ -6,8 +6,11 @@ const btnAcompCheckList = document.querySelector(".ButtonChecklist");
 
 const varHome = document.querySelector(".varHome");
 const varFaturamento = document.querySelector(".varFaturamento");
+const varFaturamento2 = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/Acompanhamento%20de%20Faturamento/AllItems.aspx?env=WebViewList&FilterField1=OI&FilterValue1=${varFaturamento}&FilterType1=Text&viewid=02e40e29%2Df33c%2D4b5b%2Db579%2D9edd42c600d5`;
 const varStatusReport = document.querySelector(".varStatusReport");
+const varStatusReport2 = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/PMO%20%20Status%20Report/Possibitar%20Cpia.aspx?FilterField1=OI&FilterValue1=${varStatusReport}&FilterType1=Text&sortField=DATASTATUSREPORT&isAscending=false&viewid=fdf0e89a%2D872a%2D4adb%2D9dfa%2D0cfc48bc813f"`;
 const varAcompSMD = document.querySelector(".varAcompSMD");
+const varAcompSMD2 = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/SEIDOR_PORTFOLIO%20%20Acompanhamento%20Solicitao%20de%20Muda/AllItems.aspx?env=WebViewList&FilterField1=OIPROJETO&FilterValue1=${varAcompSMD}&FilterType1=Text&viewid=bf33d705%2Da959%2D421c%2Dbe2a%2D1641fffb1271`;
 const varAcompCheckList = document.querySelector(".varAcompCheckList");
 
 const frame = document.querySelector(".Iframe");
@@ -21,17 +24,17 @@ function botoesFrames() {
 
   btnFaturamento.addEventListener("click", (e) => {
     e.preventDefault();
-    frame.src = varFaturamento.value;
+    frame.src = varFaturamento2;
   });
 
   btnStatusReport.addEventListener("click", (e) => {
     e.preventDefault();
-    frame.src = varStatusReport.value;
+    frame.src = varStatusReport2;
   });
 
   btnAcompSMD.addEventListener("click", (e) => {
     e.preventDefault();
-    frame.src = varAcompSMD.value;
+    frame.src = varAcompSMD2;
   });
 
   btnAcompCheckList.addEventListener("click", (e) => {
