5093b590187efe45f9a608da20771e1d22877a02
pharline <118228873+pharline@users.noreply.github.com>
2023-02-24 17:19:20 -0300

    Add files via upload

--- aplicacaov3.js
+++ aplicacaov3.js
@@ -9,13 +9,14 @@ const btnAzureDevOps = document.querySelector(".ButtonAzureDevOps");
 
 const varHome = document.querySelector(".varHome");
 const varOIPROJETO = document.querySelector(".varOIPROJETO");
+const varNomeProjeto = document.querySelector(".varNomeProjeto");
 const varAcompCheckList = document.querySelector(".varAcompCheckList");
 const varConsultasRelatorio = document.querySelector(".varConsultasRelatorio");
 const varAzureDevOps = document.querySelector(".varAzureDevOps");
 const varFaturamento = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/Acompanhamento%20de%20Faturamento/AllItems.aspx?env=WebViewList&FilterField1=OIPROJETO&FilterValue1=${varOIPROJETO.value}&FilterType1=Text&viewid=02e40e29%2Df33c%2D4b5b%2Db579%2D9edd42c600d5`;
 const varStatusReport = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/PMO%20%20Status%20Report/Possibitar%20Cpia.aspx?isAscending=false&FilterField1=OI%5FPROJETO&FilterValue1=${varOIPROJETO.value}&FilterType1=Text&sortField=DATASTATUSREPORT&viewid=fdf0e89a%2D872a%2D4adb%2D9dfa%2D0cfc48bc813f`;
 const varAcompSMD = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/SEIDOR_PORTFOLIO%20%20Acompanhamento%20Solicitao%20de%20Muda/AllItems.aspx?env=WebViewList&FilterField1=OIPROJETO&FilterValue1=${varOIPROJETO.value}&FilterType1=Text&viewid=bf33d705%2Da959%2D421c%2Dbe2a%2D1641fffb1271`;
-const varAcompCheckList2 = `https://seidor.sharepoint.com/:x:/r/sites/SeidorBR/${varAcompCheckList.value}/Documentos/50%20-%20Qualidade/${varAcompCheckList.value}_CLQ%20-%20Checklist%20Qualidade.xlsx?d=&csf=1&web=1`;
+const varAcompCheckList2 = `https://seidor.sharepoint.com/:x:/r/sites/SeidorBR/${varNomeProjeto.value}/Documentos/50%20-%20Qualidade/${varAcompCheckList.value}%20-%20Checklist%20Qualidade.xlsx?d=&csf=1&web=1`;
 const varLicaoAprendidas2 = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/SEIDOR_PMO%20%20Lies%20Aprendidas/AllItems.aspx?FilterField1=OI&FilterValue1=${varOIPROJETO.value}&FilterType1=Text&viewid=f014a450%2De380%2D4311%2Dab28%2D291c01fc3a34`;
 const varConsultasRelatorio2 = `${varConsultasRelatorio.value}`;
 const varAzureDevOps2 = `${varAzureDevOps.value}`;
@@ -60,9 +61,8 @@ function botoesFrames() {
     frame.src = varConsultasRelatorio2;
   });
 
-  btnAzureDevOps.addEventListener("click", (e) => {
-    e.preventDefault();
-    frame.src = varAzureDevOps2;
+  btnAzureDevOps.addEventListener("click", function() {
+    window.open(varAzureDevOps2);
   });
 }
 
