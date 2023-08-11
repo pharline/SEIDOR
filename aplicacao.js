443a3332d578e4e98f5c992b00b28b3596c87b03
pharline <118228873+pharline@users.noreply.github.com>
2023-08-11 16:51:34 -0300

    Update aplicacao.js

--- aplicacao.js
+++ aplicacao.js
@@ -7,7 +7,7 @@ const btnConsultasRelatorio = document.querySelector(".ButtonConsultasRelatorio"
 
 const varHome = document.querySelector(".varHome");
 const varFaturamento = document.querySelector(".varFaturamento");
-const varFaturamento2 = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/Acompanhamento%20de%20Faturamento/AllItems.aspx?env=WebViewList&FilterField1=OIPROJETO&FilterValue1=${varOIPROJETO.value}&FilterType1=Text&viewid=02e40e29%2Df33c%2D4b5b%2Db579%2D9edd42c600d5`;
+const varFaturamento2 = `https://seidor.sharepoint.com/sites/SeidorBR/projetos/Lists/PMO%20%20Fluxo%20Faturamento%20Previsto/AllItems.aspx?origin=createList&FilterField1=OIPROJETO&FilterValue1=${varOIPROJETO.value}&FilterType1=Text&viewid=3e76944c%2D76d3%2D4d85%2Da985%2Dda98ccdd8495`;
 const varStatusReport = document.querySelector(".varStatusReport");
 const varStatusReport2 = `https://seidor.sharepoint.com/sites/SeidorBR/projetos/Lists/PMO%20%20Status%20Report/AllItems.aspx?OR=Teams%2DHL&CT=1691435046155&clickparams=eyJBcHBOYW1lIjoiVGVhbXMtRGVza3RvcCIsIkFwcFZlcnNpb24iOiIyNy8yMzA3MTMyNzExNiIsIkhhc0ZlZGVyYXRlZFVzZXIiOmZhbHNlfQ%3D%3D&FilterField1=OI%5FPROJETO&FilterValue1=${varOIPROJETO.value}&FilterType1=Text&viewid=15d4e50b%2De842%2D4b20%2D98c4%2Dcaa743d64c34`;
 const varAcompSMD = document.querySelector(".varAcompSMD");
