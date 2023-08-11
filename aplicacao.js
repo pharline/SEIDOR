91e6173cdae8b127bad74680502836afbff36f6d
pharline <118228873+pharline@users.noreply.github.com>
2023-08-11 09:01:53 -0300

    Update aplicacao.js

--- aplicacao.js
+++ aplicacao.js
@@ -7,11 +7,11 @@ const btnConsultasRelatorio = document.querySelector(".ButtonConsultasRelatorio"
 
 const varHome = document.querySelector(".varHome");
 const varFaturamento = document.querySelector(".varFaturamento");
-const varFaturamento2 = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/Acompanhamento%20de%20Faturamento/AllItems.aspx?env=WebViewList&FilterField1=OI&FilterValue1=${varFaturamento.value}&FilterType1=Text&viewid=02e40e29%2Df33c%2D4b5b%2Db579%2D9edd42c600d5`;
+const varFaturamento2 = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/Acompanhamento%20de%20Faturamento/AllItems.aspx?env=WebViewList&FilterField1=OIPROJETO&FilterValue1=${varOIPROJETO.value}&FilterType1=Text&viewid=02e40e29%2Df33c%2D4b5b%2Db579%2D9edd42c600d5`;
 const varStatusReport = document.querySelector(".varStatusReport");
-const varStatusReport2 = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/PMO%20%20Status%20Report/Possibitar%20Cpia.aspx?FilterField1=OI&FilterValue1=${varStatusReport.value}&FilterType1=Text&sortField=DATASTATUSREPORT&isAscending=false&viewid=fdf0e89a%2D872a%2D4adb%2D9dfa%2D0cfc48bc813f"`;
+const varStatusReport2 = `https://seidor.sharepoint.com/sites/SeidorBR/projetos/Lists/PMO%20%20Status%20Report/AllItems.aspx?OR=Teams%2DHL&CT=1691435046155&clickparams=eyJBcHBOYW1lIjoiVGVhbXMtRGVza3RvcCIsIkFwcFZlcnNpb24iOiIyNy8yMzA3MTMyNzExNiIsIkhhc0ZlZGVyYXRlZFVzZXIiOmZhbHNlfQ%3D%3D&FilterField1=OI%5FPROJETO&FilterValue1=${varOIPROJETO.value}&FilterType1=Text&viewid=15d4e50b%2De842%2D4b20%2D98c4%2Dcaa743d64c34`;
 const varAcompSMD = document.querySelector(".varAcompSMD");
-const varAcompSMD2 = `https://seidor.sharepoint.com/teams/SeidorEscritoriodeProjetos-Brasil.br/Lists/SEIDOR_PORTFOLIO%20%20Acompanhamento%20Solicitao%20de%20Muda/AllItems.aspx?env=WebViewList&FilterField1=OIPROJETO&FilterValue1=${varAcompSMD.value}&FilterType1=Text&viewid=bf33d705%2Da959%2D421c%2Dbe2a%2D1641fffb1271`;
+const varAcompSMD2 = `https://seidor.sharepoint.com/sites/SeidorBR/projetos/Lists/PMO%20%20Solicitao%20de%20Mudana%20%20SMD/AllItems.aspx?origin=createList&useFiltersInViewXml=1&FilterField1=OIPROJETO&FilterValue1=${varOIPROJETO.value}&FilterType1=Text&viewid=35e8f5f6%2Ddb2e%2D413b%2Dbe14%2D5739bd2ac755`;
 const varAcompCheckList = document.querySelector(".varAcompCheckList");
 const varConsultasRelatorio = document.querySelector(".varConsultasRelatorio");
 
